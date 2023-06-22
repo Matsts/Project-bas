575d76363dcdee7bec0b05c6514ca54f5102e242
Mees <meesjvdhulst@outlook.com>
2023-06-22 20:43:22 +0200

    commit

---
+++ dev.test.nl-main/index.php
@@ -0,0 +1,11 @@
+<img src="https://seeklogo.com/images/B/bas-logo-8659E994AF-seeklogo.com.gif" alt="bas logo" width="125" height="150"></br>
+<form action="insertKlant.php"><input type="submit" value="Nieuwe klant toevoegen" /></form>
+<form action="selectklant.php"><input type="submit" value="Klanten inkijken" /></form>
+<form action="insertverkoop.php"><input type="submit" value="Nieuwe verkooporder toevoegen" /></form>
+<form action="selectVerkoop.php"><input type="submit" value="verkoop orders inkijken" /></form>
+<form action="insertinkoop.php"><input type="submit" value="Nieuwe inkooporders toevoegen" /></form>
+<form action="deleteinkoop.php"><input type="submit" value="Inkoop orders verwijderen" /></form>
+<form action="selectartikel.php"><input type="submit" value="alle artikelen inkijken" /></form>
+<form action="deleteleverancier.php"><input type="submit" value="Leverancier verwijderen" /></form>
+<form action="searchklant.php"><input type="submit" value="klanten zoeken" /></form>
+<form action="updatestatus.php"><input type="submit" value="Verkoop order status wijzigen" /></form>
